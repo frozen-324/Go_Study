29b7326fdd978314676db04b8be9052ef38cedcf
unknown <davjiang324@gmail.com>
2025-08-10 15:13:00 -0700

    Go Quick Guide

---
+++ hello/hello.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+
+	// message, err := greetings.Hello("Gladys")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// message := greetings.Hello("Gladys")
+	fmt.Println(messages)
+}
